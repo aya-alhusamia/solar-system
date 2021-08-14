6d9ec5e80772141b18d158677b2e3799baab0a1f
aya-alhusamia <alhusamiaya70@gmail.com>
2021-08-14 14:21:41 +0300

    authentication-is-done

--- src/Component/Authentication/Signin.js
+++ src/Component/Authentication/Signin.js
@@ -2,101 +2,88 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 
-//Components
+//Action
 import { signin } from "../../store/action/userActions";
 
-//Styling
+//Component
+import Solar from "./Solar/Solar"
+
+//Style
 import { AiFillEye } from "react-icons/ai";
 import { AiFillEyeInvisible } from "react-icons/ai";
 import { FiUser } from "react-icons/fi";
-import { UserForm } from "../../styles";
-import './style.scss'
+import "./style.css"
 
-const SignIn = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const [pass, setpass] = useState("password");
-  const [user, setUser] = useState({
-    username: "",
-    password: "",
-  });
-  const [focus, setFocus] = useState("")
-  const handleChange = (event) =>
-    setUser({
-      ...user,
-      [event.target.name]: event.target.value,
+function Signin() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const [pass, setpass] = useState("password");
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
     });
-  const showPass = () => {
-    pass === "password" ? setpass("text") : setpass("password");
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(signin(user, history));
-  };
+    const [focus, setFocus] = useState("")
+    const handleChange = (event) =>
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value,
+        });
+    const showPass = () => {
+        pass === "password" ? setpass("text") : setpass("password");
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        dispatch(signin(user, history));
+    };
+    return (
+        <div className="test">
+            <Solar />
+            <div className="log-from">
+                <form className="container" onSubmit={handleSubmit}>
+                    <h2>LOGIN</h2>
+                    <div className="lif">
+                        <input type='text'
+                            className="login-element"
+                            placeholder="Username"
+                            onChange={handleChange}
+                            value={user.username}
+                            name="username" />
+                        <span class="input-group-text" id="basic-addon2">
+                            <FiUser size="1.5em" color="#e57300"
+                                shadow="0 1px 1px #e5e500, 0 2px 2px #e57300" />
+                        </span>
 
+                    </div>
+                    <div className="lif">
+                        <input type={pass}
+                            className="login-element"
+                            placeholder="Password"
+                            onChange={handleChange}
+                            value={user.password}
+                            name="password" />
+                        <span class="input-group-text" id="basic-addon2">
+                            {pass === "password" ? (
+                                <AiFillEye size="1.5em" onClick={showPass} color="#e57300" shadow="0 1px 1px #e5e500, 0 2px 2px #e57300" />
+                            ) : (
+                                <AiFillEyeInvisible size="1.5em" onClick={showPass} color="#e57300" shadow="0 1px 1px #e5e500, 0 2px 2px #e57300" />
+                            )}
+                        </span>
 
-
-  return (
-
-
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-4"></div>
-        <div className="col-4">
-          {" "}
-          <UserForm onSubmit={handleSubmit}>
-            <h1 className="mb-5">Sign in</h1>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Username"
-                aria-label="Username"
-                aria-describedby="basic-addon2"
-                onChange={handleChange}
-                value={user.username}
-                name="username"
-              />
-              <span class="input-group-text" id="basic-addon2">
-                <FiUser size="1.5em" />
-              </span>{" "}
+                    </div>
+                    <input type="submit"
+                        id="login-btn"
+                        value="Login" />
+                </form>
+                <div className="test3">
+                    <p>if you  dont have accout! please sign up  <Link to="/signup" >
+                        here
+                    </Link></p>
+                </div>
             </div>
-            <div class="input-group mb-3">
-              <input
-                type={pass}
-                class="form-control"
-                placeholder="Password"
-                aria-label="Password"
-                aria-describedby="basic-addon2"
-                onChange={handleChange}
-                value={user.password}
-                name="password"
-              />
-              <span class="input-group-text" id="basic-addon2">
-                {pass === "password" ? (
-                  <AiFillEye size="1.5em" onClick={showPass} />
-                ) : (
-                  <AiFillEyeInvisible size="1.5em" onClick={showPass} />
-                )}
-              </span>{" "}
-            </div>
-            <button
-              type="submit"
-              className="btn btn-primary btn-block mb-4 float-end"
-            >
-              Sign in
-            </button>
-          </UserForm>
+
         </div>
-        <div className="col-4"></div>
-        <p>dont have accout! please sign up  <Link to="/signup" >
-          here
-        </Link></p>
-      </div>
-    </div>
 
+    )
+}
 
-  );
-};
-
-export default SignIn;
+export default Signin
--- src/Component/Routes.js
+++ src/Component/Routes.js
@@ -1,25 +1,38 @@
-import { Route, Switch } from "react-router";
+import { Redirect, Route, Switch } from "react-router";
 
 //components
 import SignUp from "./Authentication/Signup";
-import SignIn from "./Authentication/Signin";
+import Signin from "./Authentication/Signin";
 import Puzzle from "./Puzzle/Puzzle";
+import Navbar from "./Navbar/Navbar";
+import ErrorPage from "./404Page/ErrorPage";
 
 function Routes() {
   return (
     <Switch>
+
+
       <Route path="/signup">
         <SignUp />
       </Route>
-
       <Route exact path="/signin">
-        <SignIn />
+        <Signin />
       </Route>
       <Route exact path="/games">
         <Puzzle />
       </Route>
+      <Route exact path="/navbar">
+        <Navbar />
+      </Route>
+      {/* <Route exact path="/error">
+        <ErrorPage />
+      </Route> */}
+      <Redirect to="/" />
+
+
     </Switch>
   );
 }
+// عند الريدايركت بحط 404 بيج اوووووووووووووووووووك 
 
 export default Routes;
