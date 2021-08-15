4bd00aae3c2f599f46aecf7b0248b938e6f548ca
esraamalkawi <esraamalkawi462@gmail.com>
2021-08-15 04:58:10 -0700

    delete

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,8 +1,7 @@
-
 import React from "react";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import { HiOutlineLogout } from "react-icons/hi";
-import { GiSpaceSuit} from 'react-icons/gi';
+import { GiSpaceSuit } from "react-icons/gi";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { signout } from "../../store/action/authActions";
@@ -11,13 +10,15 @@ import { useHistory } from "react-router-dom";
 import { Navtitle } from "../../styles";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({btn: {
+const useStyles = makeStyles(() => ({
+  btn: {
     fontSize: 18,
     color: "white",
     "&:hover": {
       backgroundColor: "grey",
     },
-  }}));
+  },
+}));
 
 const Navbar = () => {
   const user = useSelector((state) => state.user.user);
@@ -31,20 +32,18 @@ const Navbar = () => {
   return (
     <div>
       <nav
-         className="navbar navbar-expand-sm bg-dark navbar-light"
+        className="navbar navbar-expand-sm bg-dark navbar-light"
         style={{ backgroundColor: "#495057" }}
       >
         <div className="container-fluid">
           <Navtitle className="navbar-brand" style={{ color: "#f8f9fa" }}>
             <GiSpaceSuit size="2.0em" />
-          Crisis Galaxy
+            Crisis Galaxy
           </Navtitle>
           <ul className="navbar-nav">
             <NavLink to="/">
-                <li className="nav-item active" >
-              <Button className={classes.btn}>
-                Home
-              </Button>
+              <li className="nav-item active">
+                <Button className={classes.btn}>Home</Button>
               </li>
             </NavLink>
             {/* {user && user.userType === "admin" ? (
@@ -58,18 +57,23 @@ const Navbar = () => {
             )} */}
 
             {user && (
-              <NavLink to="/games">
-                   <li className="nav-item" >
-                <Button className={classes.btn} >
-                  Games
-                </Button>
-                </li>
-              </NavLink>
+              <>
+                <NavLink to="/games">
+                  <li className="nav-item">
+                    <Button className={classes.btn}>Games</Button>
+                  </li>
+                </NavLink>
+                <NavLink to="/crisisstore">
+                  <li className="nav-item">
+                    <Button className={classes.btn}>Store</Button>
+                  </li>
+                </NavLink>
+              </>
             )}
             {user ? (
               <>
                 <NavLink to="/games">
-                  <li  className="nav-item" style={{ paddingTop: "10px" }}>
+                  <li className="nav-item" style={{ paddingTop: "10px" }}>
                     <p
                       style={{
                         marginLeft: "5px",
@@ -94,17 +98,13 @@ const Navbar = () => {
             ) : (
               <>
                 <NavLink to="/signin">
-                    <li className="nav-item">
-                  <Button className={classes.btn}>
-                    Sign in
-                  </Button>
+                  <li className="nav-item">
+                    <Button className={classes.btn}>Sign in</Button>
                   </li>
                 </NavLink>
                 <NavLink to="/signup">
-                <li className="nav-item">
-                  <Button className={classes.btn}>
-                    Sign up
-                  </Button>
+                  <li className="nav-item">
+                    <Button className={classes.btn}>Sign up</Button>
                   </li>
                 </NavLink>
               </>
