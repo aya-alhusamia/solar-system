e5ae612dd530d3e988d8786f0f4345abb0232fef
aya-alhusamia <alhusamiaya70@gmail.com>
2021-08-18 16:25:12 +0300

    add solar solar

---
+++ src/Component/Solar-System/pages/Saturn/Saturn.js
@@ -0,0 +1,8 @@
+import PlanetSection from '../../SolarSystem/PlanetSection/PlanetSection';
+import { data } from './data';
+
+const Saturn = () => {
+    return <PlanetSection planetData={data} />;
+};
+
+export default Saturn;
