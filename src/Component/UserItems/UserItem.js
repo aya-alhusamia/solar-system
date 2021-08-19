694a6adeab0fd827700efc4a4dede83d956c1f1c
esraamalkawi <esraamalkawi462@gmail.com>
2021-08-19 15:02:10 -0700

    try

--- src/Component/UserItems/UserItem.js
+++ src/Component/UserItems/UserItem.js
@@ -1,16 +1,12 @@
 import { useSelector } from "react-redux";
 
-
-
-
 const UserItem = (props) => {
   const items = useSelector((state) => state.items.items);
-  const specificItem = items.find((item) => item.id === props.userItem.itemId)
-  console.log(specificItem)
+  const specificItem = items.find((item) => item.id === props.userItem.itemId);
   return (
     <div className="it">
       <h3>{props.userItem.name}</h3>
-      <img className="image" src={specificItem.image} />
+      <img className="image" src={specificItem?.image} />
     </div>
   );
 };
--- src/Component/UserItems/UserItemList.js
+++ src/Component/UserItems/UserItemList.js
@@ -4,21 +4,42 @@ import { Redirect } from "react-router-dom";
 import UserItem from "./UserItem";
 import Orbits from "./Orbits/Orbits";
 //Style
-import "./style.css"
+import "./style.css";
 const UserItemList = () => {
   const userItems = useSelector((state) => state.userItems.userItems);
-  console.log("userItems", userItems);
   const user = useSelector((state) => state.user.user);
 
   let userItemsList = userItems.map((userItem) => (
     <UserItem userItem={userItem} key={userItem.id} />
   ));
+  const e = userItemsList[0].props;
+  console.log(e);
+
   if (!user) return <Redirect to="/" />;
+
+  const allowDrop = (e) => {
+    e?.preventDefault();
+    console.log("allow drop", e);
+  };
+
+  const drag = (e) => {
+    e?.dataTransfer.setData("text", e.target.id);
+    console.log("drag", e);
+  };
+
+  const drop = (e) => {
+    e?.preventDefault();
+    let data = e?.dataTransfer.getData("text");
+    e?.target.appendChild(document.getElementById(data));
+    console.log("drop", e);
+  };
 
   return (
     <div className="item-list">
-      <Orbits />
-      {userItemsList}
+      <Orbits id="div1" onDrop={drop()} onDragOver={allowDrop()} />
+      <div id="drag1" draggable="true" onDragStart={drag()}>
+        {userItemsList}
+      </div>
     </div>
   );
 };
